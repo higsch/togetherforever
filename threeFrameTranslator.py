9e19e9fead50de164bd5cb82e68152c8fc58f6e2
Matthias Stahl <matthias.stahl87@web.de>
2019-01-24 16:37:39 +0100

    fixed bug with argument handling in translator

--- threeFrameTranslator.py
+++ threeFrameTranslator.py
@@ -6,19 +6,10 @@ import glob
 import argparse
 from Bio import SeqIO
 
-def get_abspath(p):
-  if not os.path.exists(os.path.abspath(os.path.expanduser(p))):
-    if len(glob.glob(p))>0:
-      return p
-    raise argparse.ArgumentTypeError(p)
-  if os.path.isdir(os.path.abspath(os.path.expanduser(p))):
-    return os.path.abspath(os.path.expanduser(p))+'/'
-  return os.path.abspath(os.path.expanduser(p))
-
 def parseCommandlineArgs():
   parser = argparse.ArgumentParser(description = '3-frame translation for nucleotide sequences.')
-  parser.add_argument('-i', '--input_file', type = get_abspath, default = "", required = True, help = 'input fasta')
-  parser.add_argument('-o', '--output_file', type = get_abspath, default = "", required = True, help = 'output fasta')
+  parser.add_argument('-i', '--input-file', dest = "infile", default = "", required = True, help = 'input fasta')
+  parser.add_argument('-o', '--output-file', dest = "outfile", default = "", required = True, help = 'output fasta')
   return parser.parse_args(sys.argv[1:])
 
 def translate(infile, outfile):
@@ -37,4 +28,4 @@ def translate(infile, outfile):
 
 if __name__ == '__main__':
   args = parseCommandlineArgs()
-  translate(args.input_file, args.output_file)
+  translate(args.infile, args.outfile)
